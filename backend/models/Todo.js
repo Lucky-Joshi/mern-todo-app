f6cf464d6c7d30e92cd8095ffe6a55a25539bdfa
Lucky Joshi <luckyjoshi524@gmail.com>
2025-07-09 08:16:06 +0530

    Todo.js

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const TodoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
+  priority: {
+    type: String,
+    enum: ["High", "Medium", "Low", "Lowest"],
+    default: "Medium"
+  },
   isCompleted: { type: Boolean, default: false },
   isTrashed: { type: Boolean, default: false },
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('Todo', TodoSchema);
